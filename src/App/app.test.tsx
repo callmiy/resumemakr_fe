7adc9b0c35007a0f4dc3b852ccfebceb600b65e4
maneptha <maneptha@gmail.com>
2019-01-10 03:40:45 +0100

    App - test that cache persist is called.

--- src/App/app.test.tsx
+++ src/App/app.test.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import { render, wait } from "react-testing-library";
 
 import App from "./app-x";
-import { renderWithApollo, renderWithRouter } from "../test_utils";
+import { renderWithApollo } from "../test_utils";
 import { Props } from "./app";
 
 const AppP = App as React.ComponentClass<Partial<Props>>;
 
 it("renders without crashing", async () => {
-  const { Ui: ui } = renderWithRouter(AppP);
-  const { Ui } = renderWithApollo(ui);
-  const { unmount } = render(<Ui persistCache={jest.fn()} />);
+  const { Ui } = renderWithApollo(AppP);
+  const persistCache = jest.fn();
+  const { unmount } = render(<Ui persistCache={persistCache} />);
+  await wait(() => expect(persistCache).toBeCalled());
   await wait(() => expect(unmount()).toBe(true));
 });
